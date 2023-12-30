c7f7c238d0e08c214ac7469fa02d07b579b4e0da
Tirumalasetty Ashok <147503242+Tiru373@users.noreply.github.com>
2023-12-30 13:42:01 +0530

    Update digital_clock.v

--- digital_clock.v
+++ digital_clock.v
@@ -1,44 +1,37 @@
-module digital_clock_();
-  // Import the design to be tested
- import digital_clock (
-    input clk,
-    input rst,
-    output reg [4:0] hour,
-    output reg [5:0] minute,
-    output reg [50] second
-  );
-
-  // any necessary signals or variables
-  reg clk;
-  reg rst;
-
- // Create any necessary clocks or reset signals
-  always #10 clk = ~clk;
-
-  // Initialize the design
-  initial begin
-    clk = 1'b0;
-    rst = 1'b1;
-    #100;
-    rst = 1'b0;
-  end
-
-  // Apply stimulus to the design
-  initial begin
-    // No stimulus needed for a clock
-  end
-
-  // Connect the testbench to the design
-  always @(posedge clk) begin
-    // Connect input signals here
-    digital_clock.clk <= clk;
-    digital_clock.rst <= rst;
-  end
-
-  // Monitor the design's output
-  initial begin
-    // Monitor output signals here
-    $monitor("Time = %t, Clock = %b, Reset = %b, Hour = %d, Minute = %d, Second = %d", $time, clk, rst, hour, minute, second);
-  end
-
+module digiclock(
+    input clk,rst,
+    output reg[5:0] sec,min,
+     output reg[4:0] hr
+    );
+always@(posedgeclk or posedgerst)
+begin
+     if(!rst)
+      begin
+      sec=0;
+      min=0;
+    hr=0;
+      end
+    
+      else if(clk==1)
+      begin
+      sec=sec+1;
+    
+       if (sec==60)
+        begin
+        sec=0;
+        min=min+1;
+        
+          if(min==60)
+          begin
+          min=0;
+        hr=hr+1;
+        
+              if(hr==24)
+              begin
+            hr=0;
+              end
+          end
+        end
+      end
+end 
 endmodule
